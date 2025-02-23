8d1d82ad7f932d16f282e69039cb6f3b52894c42
TH_97 <hood3392@gmail.com>
2025-02-23 16:10:33 +0900

    Initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { routers } from "./routers";
+
+const router = createBrowserRouter(routers);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
---
+++ src/routers.tsx
@@ -0,0 +1,8 @@
+import Home from "./routes/home";
+
+export const routers = [
+  {
+    path: "/",
+    element: <Home />,
+  },
+];
