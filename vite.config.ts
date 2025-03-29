d7a29ee54b8160545b2cf5b1e68c22d6b91c1a8e
TH_97 <hood3392@gmail.com>
2025-03-29 18:15:30 +0900

    feat : docker 이미지를 만들기 위한 dockerfile , dockerignore 추가 fix : 포트번호 지정을 위한  vite.config.ts 수정, package.json 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src/"), // '@shared'를 'src/shared'로 맵핑
-    },
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
   },
+  plugins: [react()],
 });
